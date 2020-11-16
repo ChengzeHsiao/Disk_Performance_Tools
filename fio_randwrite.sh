198433fe6674f56f2f03d7763bc15954f1648ed1
HugoHsiao <60311214+Hugo-Hsiao@users.noreply.github.com>
2020-11-16 11:52:07 +0800

    Add files via upload

---
+++ fio_randwrite.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#mun hosts fio 有多少个fio主机，添加几个，./==.fio文件
+fio --client=192.168.115.50,8765 /root/randwrite.fio --client=192.168.115.51,8765 /root/randwrite.fio --client=192.168.115.52,8765 /root/randwrite.fio --client=192.168.115.53,8765 /root/randwrite.fio --client=192.168.115.54,8765 /root/randwrite.fio --client=192.168.115.55,8765 /root/randwrite.fio
